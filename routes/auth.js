cc738e70cd4c3e17b29fc09d8bdb5077b74e5aa2
julian-guerra92 <julianguerra92@gmail.com>
2022-09-16 18:09:13 -0500

    Google Sing added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -11,4 +11,9 @@ router.post('/login', [
     validarCampos
 ], login);
 
+router.post('/google', [
+    check('id_token', 'ID Token es necesario').not().isEmpty(),
+    validarCampos
+], googleSingIn);
+ 
 module.exports = router; 
